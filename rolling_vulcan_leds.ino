54dd56e175857faddfc630361f82a4ddc93dbb13
Mayara Gouveia <35708820+gb8may@users.noreply.github.com>
2025-04-02 19:05:48 -0400

    Create rolling_vulcan_leds.ino

---
+++ rolling_vulcan_leds.ino
@@ -0,0 +1,21 @@
+const int leds[] = {2, 3, 4, 5, 6};
+const int numLeds = 5;
+
+void setup() {
+  for (int i = 0; i < numLeds; i++) {
+    pinMode(leds[i], OUTPUT);
+  }
+
+  randomSeed(analogRead(0));
+}
+
+void loop() {
+  int ledIndex = random(numLeds);
+  int tempoPiscar = random(10, 100);
+
+  digitalWrite(leds[ledIndex], HIGH);
+  delay(tempoPiscar);
+  digitalWrite(leds[ledIndex], LOW);
+
+  delay(random(1, 5));
+}
